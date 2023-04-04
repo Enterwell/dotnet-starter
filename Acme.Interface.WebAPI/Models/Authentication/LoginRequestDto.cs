4b7b0b469be7dd569d3cc9f0ab75fa22bea0c1cd
DorijanH <dhaspl@gmail.com>
2023-04-04 13:53:02 +0200

    Added initial migration

--- Acme.Interface.WebAPI/Models/Authentication/LoginRequestDto.cs
+++ Acme.Interface.WebAPI/Models/Authentication/LoginRequestDto.cs
@@ -8,10 +8,12 @@ public class LoginRequestDto
     /// <summary>
     /// Gets or sets the user's email.
     /// </summary>
+    /// <example>admin@acme.com</example>
     public string Email { get; set; }
 
     /// <summary>
     /// Gets or sets the user's password.
     /// </summary>
+    /// <example>pa$$w0rd</example>
     public string Password { get; set; }
 }
