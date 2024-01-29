91001fe952835a559111ceea0bad2fc46deae0b2
Dubravko Rittuper <dubravko.rittuper@enterwell.net>
2024-01-29 11:46:12 +0100

    #16 Added app and db health checks

---
+++ Acme.Interface.WebAPI/Configuration/InstallerExtensions/HealthCheckServicesExtensions.cs
@@ -0,0 +1,28 @@
+﻿using HealthChecks.ApplicationStatus.DependencyInjection;
+
+namespace Acme.Interface.WebAPI.Configuration.InstallerExtensions;
+
+/// <summary>
+/// Health check services extensions.
+/// </summary>
+public static class HealthCheckServicesExtensions
+{
+    /// <summary>
+    /// Adds the custom health checks.
+    /// </summary>
+    /// <param name="services">The services.</param>
+    /// <param name="configuration">The configuration.</param>
+    /// <returns></returns>
+    /// <exception cref="System.ArgumentNullException"></exception>
+    public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    {
+        string postgreSqlConnString = configuration.GetValue<string>("PostgreSQL:ConnectionString") ?? throw new ArgumentNullException();
+
+        services
+            .AddHealthChecks()
+            .AddApplicationStatus("WebAPI", tags: ["api"])
+            .AddNpgSql(postgreSqlConnString, name: "PostgreSQL", tags: ["database"]);
+
+        return services;
+    }
+}
---
+++ Acme.Interface.WebAPI/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+
+namespace Acme.Interface.WebAPI.Extensions;
+
+/// <summary>
+/// Health check extensions.
+/// </summary>
+public static class HealthCheckExtensions
+{
+    /// <summary>
+    /// Maps the health checks with json response.
+    /// </summary>
+    /// <param name="endpoints">The endpoints.</param>
+    /// <param name="path">The path.</param>
+    public static void MapHealthChecksWithJsonResponse(this IEndpointRouteBuilder endpoints, PathString path)
+    {
+        var options = new HealthCheckOptions
+        {
+            ResponseWriter = async (httpContext, healthReport) =>
+            {
+                httpContext.Response.ContentType = "application/json";
+
+                var result = JsonConvert.SerializeObject(new
+                {
+                    status = healthReport.Status.ToString(),
+                    totalDurationInMs = Math.Round(healthReport.TotalDuration.TotalMilliseconds, 2),
+                    entries = healthReport.Entries.Select(e => new
+                    {
+                        name = e.Key,
+                        status = e.Value.Status.ToString(),
+                        description = e.Value.Description,
+                        durationInMs = Math.Round(e.Value.Duration.TotalMilliseconds, 2),
+                        data = e.Value.Data,
+                        tags = e.Value.Tags
+                    }),
+                });
+
+                await httpContext.Response.WriteAsync(result);
+            }
+        };
+
+        endpoints.MapHealthChecks(path, options);
+    }
+}
